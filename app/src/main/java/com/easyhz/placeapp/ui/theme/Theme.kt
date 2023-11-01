dc05fc3932cc9bb7d4229825610339fb3a9d555c
easyhz <jiton3@naver.com>
2023-11-01 22:03:40 +0900

    Update: Theme 설정 1. 색상 및 테마 적용 2. NoRipple 적용

--- app/src/main/java/com/easyhz/placeapp/ui/theme/Theme.kt
+++ app/src/main/java/com/easyhz/placeapp/ui/theme/Theme.kt
@@ -1,45 +1,71 @@
 package com.easyhz.placeapp.ui.theme
 
+import android.annotation.SuppressLint
 import android.app.Activity
 import android.os.Build
+import android.util.Log
 import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material.ripple.LocalRippleTheme
+import androidx.compose.material.ripple.RippleAlpha
+import androidx.compose.material.ripple.RippleTheme
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.ReadOnlyComposable
 import androidx.compose.runtime.SideEffect
+import androidx.compose.runtime.staticCompositionLocalOf
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Color.Companion.Transparent
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+    primary = Neutral0,
+    onPrimary = Neutral8,
+    secondary = Neutral0,
+    onSecondary = Neutral8,
+    tertiary = Neutral0,
+    onTertiary = Neutral8,
+    error = FunctionalRed,
+    onError = Neutral8,
+    background = Neutral8,
+    onBackground = Neutral1,
+    outline = Neutral0,
+    surface = Neutral8,
+    surfaceVariant = Neutral8,
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
-
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
+    primary = Neutral8,
+    onPrimary = Neutral0,
+    secondary = Neutral8,
+    onSecondary = Neutral0,
+    tertiary = Neutral8,
+    onTertiary = Neutral0,
+    error = FunctionalRed,
+    onError = Neutral0,
+    background = Neutral1,
+    onBackground = Neutral8,
+    outline = Neutral8,
+    surface = Neutral0,
+    surfaceVariant = Neutral0,
+    secondaryContainer = Transparent,
+    onSecondaryContainer = Transparent
 )
+
+private val AppLightThemeColors = appLightColorScheme()
+private val AppDarkThemeColors = appDarkColorScheme()
 
 @Composable
 fun PlaceAppTheme(
-    darkTheme: Boolean = isSystemInDarkTheme(),
+    isDarkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
     dynamicColor: Boolean = true,
     content: @Composable () -> Unit
@@ -47,24 +73,149 @@ fun PlaceAppTheme(
     val colorScheme = when {
         dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
             val context = LocalContext.current
-            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
+            if (isDarkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
         }
 
-        darkTheme -> DarkColorScheme
+        isDarkTheme -> DarkColorScheme
         else -> LightColorScheme
     }
+
+    val appColorScheme = when {
+        isDarkTheme -> AppDarkThemeColors
+        else -> AppLightThemeColors
+    }
+
     val view = LocalView.current
     if (!view.isInEditMode) {
         SideEffect {
             val window = (view.context as Activity).window
-            window.statusBarColor = colorScheme.primary.toArgb()
-            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = darkTheme
+            window.statusBarColor = appColorScheme.transparent.toArgb()
+            window.navigationBarColor = appColorScheme.transparent.toArgb()
+            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = !isDarkTheme
         }
     }
 
     MaterialTheme(
         colorScheme = colorScheme,
         typography = Typography,
+        content = {
+            ProvidePlaceAppTheme(appColorScheme = appColorScheme) {
+                Surface(content = content)
+            }
+        },
+    )
+}
+
+object PlaceAppTheme {
+    val colorScheme: PlaceAppColors
+        @Composable
+        @ReadOnlyComposable
+        get() = CustomLocalColorScheme.current
+}
+
+@Composable
+fun ProvidePlaceAppTheme(
+    appColorScheme: PlaceAppColors,
+    content: @Composable () -> Unit
+) {
+    CompositionLocalProvider(
+        CustomLocalColorScheme provides appColorScheme,
         content = content
     )
+    CompositionLocalProvider(
+        LocalRippleTheme provides NoRippleTheme,
+        content = content
+    )
+
+}
+
+/**
+ * primary : 기본 색상
+ * secondary : 보조 색상
+ * transparent : 투명
+ * primaryBorder: border 기본 색상
+ * selectedIcon: 선택 아이콘 색상
+ * unselectedIcon: 미선택 아이콘 색상
+ * background: 배경 색상
+ */
+open class PlaceAppColors (
+    val primary: Color,
+    val secondary: Color,
+    val transparent: Color,
+    val primaryBorder: Color,
+    val selectedIcon: Color,
+    val unselectedIcon: Color,
+    val background: Color,
+)
+
+object AppLightColors: PlaceAppColors(
+    primary = Sky1,
+    secondary = Sky3,
+    transparent = Transparent,
+    primaryBorder = Neutral4,
+    selectedIcon = Neutral8,
+    unselectedIcon = Neutral5,
+    background = Neutral1
+)
+
+object AppDarkColors : PlaceAppColors(
+    primary = Sky3,
+    secondary = Sky1,
+    transparent = Transparent,
+    primaryBorder = Neutral4,
+    selectedIcon = Neutral0,
+    unselectedIcon = Neutral3,
+    background = Neutral8
+)
+
+fun appLightColorScheme(
+    primary: Color = AppLightColors.primary,
+    secondary: Color = AppLightColors.secondary,
+    transparent: Color = AppLightColors.transparent,
+    primaryBorder: Color = AppLightColors.primaryBorder,
+    selectedIcon: Color = AppLightColors.selectedIcon,
+    unselectedIcon: Color = AppLightColors.unselectedIcon,
+    background: Color = AppLightColors.background
+) : PlaceAppColors =
+    PlaceAppColors (
+        primary,
+        secondary,
+        transparent,
+        primaryBorder,
+        selectedIcon,
+        unselectedIcon,
+        background
+    )
+
+fun appDarkColorScheme(
+    primary: Color = AppDarkColors.primary,
+    secondary: Color = AppDarkColors.secondary,
+    transparent: Color = AppDarkColors.transparent,
+    primaryBorder: Color = AppDarkColors.primaryBorder,
+    selectedIcon: Color = AppDarkColors.selectedIcon,
+    unselectedIcon: Color = AppDarkColors.unselectedIcon,
+    background: Color = AppDarkColors.background
+) : PlaceAppColors =
+    PlaceAppColors (
+        primary,
+        secondary,
+        transparent,
+        primaryBorder,
+        selectedIcon,
+        unselectedIcon,
+        background
+    )
+
+@SuppressLint("CompositionLocalNaming")
+internal val CustomLocalColorScheme = staticCompositionLocalOf { appLightColorScheme() }
+
+/**
+ * NoRipple
+ */
+private object NoRippleTheme : RippleTheme {
+    @Composable
+    override fun defaultColor() = Color.Unspecified
+
+    @Composable
+    override fun rippleAlpha(): RippleAlpha = RippleAlpha(0.0f,0.0f,0.0f,0.0f)
 }
