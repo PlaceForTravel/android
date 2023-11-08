f21eff9b1db59f1ca5461288767003dab3609d2e
easyhz <jiton3@naver.com>
2023-11-08 23:39:20 +0900

    Fix: PaddingConstants 추가

---
+++ app/src/main/java/com/easyhz/placeapp/constants/PaddingConstants.kt
@@ -0,0 +1,11 @@
+package com.easyhz.placeapp.constants
+
+object PaddingConstants {
+    const val CONTENT_ALL = 10
+    const val CONTENT_HORIZONTAL = 10
+    const val CONTENT_VERTICAL = 10
+    const val ICON_TEXT_HORIZONTAL = 20
+    const val ICON_TEXT_VERTICAL = 10
+    const val TEXT_HORIZONTAL = 25
+    const val IMAGE_HORIZONTAL = 10
+}
