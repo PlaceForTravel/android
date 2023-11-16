18b3e517424ea439a2f3ea64824cbeaa94d48ac9
easyhz <jiton3@naver.com>
2023-11-16 14:51:10 +0900

    Fix: New Post 개선 (UI 적용 전)

--- app/src/main/java/com/easyhz/placeapp/ui/post/NewPost.kt
+++ app/src/main/java/com/easyhz/placeapp/ui/post/NewPost.kt
@@ -1,25 +1,47 @@
 package com.easyhz.placeapp.ui.post
 
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
+import com.easyhz.placeapp.R
+import com.easyhz.placeapp.ui.component.post.PostHeader
+import com.easyhz.placeapp.ui.theme.PlaceAppTheme
+import com.easyhz.placeapp.util.borderBottom
 
 //const val GRID_CELL = 4
 @Composable
 fun NewPost(
-    viewModel: NewPostViewModel = hiltViewModel()
+    viewModel: NewPostViewModel = hiltViewModel(),
+    onNavigateToBack: () -> Unit,
+    onNavigateToNext: () -> Unit,
 ) {
 
-    Box(
-        modifier = Modifier.fillMaxSize(),
-        contentAlignment = Alignment.TopCenter,
-    ) {
-        Text(text = "NewPost")
+    Column {
+        PostHeader(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(50.dp)
+                .borderBottom(color = PlaceAppTheme.colorScheme.primaryBorder, dp = 1.dp),
+            title = stringResource(id = R.string.post_new_post_header),
+            onBackClick = { onNavigateToBack() },
+            onNextClick = { onNavigateToNext() }
+        )
+        Box(
+            modifier = Modifier.fillMaxSize(),
+            contentAlignment = Alignment.TopCenter,
+        ) {
+            Text(text = "NewPost")
+        }
     }
 }
 
