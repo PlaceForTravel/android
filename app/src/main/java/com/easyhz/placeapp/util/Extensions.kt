76e938cbfafa8136af7db43bc28e5d2181c6ad22
easyhz <jiton3@naver.com>
2023-11-22 15:32:47 +0900

    Update: Extension Test 추가

--- app/src/main/java/com/easyhz/placeapp/util/Extensions.kt
+++ app/src/main/java/com/easyhz/placeapp/util/Extensions.kt
@@ -3,6 +3,8 @@ package com.easyhz.placeapp.util
 import android.database.Cursor
 import androidx.core.text.HtmlCompat
 import androidx.core.text.HtmlCompat.FROM_HTML_MODE_LEGACY
+import java.math.BigDecimal
+import java.math.RoundingMode
 
 fun Cursor.getLongColumnOrThrow(columnName: String): Long =
     getLong(getColumnIndexOrThrow(columnName))
@@ -14,3 +16,7 @@ fun Cursor.getStringColumnOrThrow(columnName: String): String? =
 fun String.toBasicCategory() = this.split(">").last()
 
 fun String.withoutHTML() = HtmlCompat.fromHtml(this, FROM_HTML_MODE_LEGACY).toString()
+
+fun Double.toLatLng(newScale: Int) = BigDecimal(this).setScale(newScale, RoundingMode.HALF_UP).toDouble()
+
+fun String.toAddress() = this.split(" ").slice(0..1).joinToString(" ")
