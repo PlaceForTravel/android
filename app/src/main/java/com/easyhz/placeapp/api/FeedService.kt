34e4e48cc64034ac74eba88e7e68a4a55a285902
easyhz <jiton3@naver.com>
2023-11-28 18:16:21 +0900

    update: api

--- app/src/main/java/com/easyhz/placeapp/api/FeedService.kt
+++ app/src/main/java/com/easyhz/placeapp/api/FeedService.kt
@@ -16,26 +16,38 @@ interface FeedService {
         @Query("page") page: Int
     ) : Response<Feed>
 
-    /* 글 상세 조회 */
+    /* 글 상세 조회 (수정 필요) */
     @GET("/board/detail/{boardId}")
     suspend fun getBoardDetail(
         @Path("boardId") id: Int
     )
 
-    /* 댓글 조회 */
+    /* 댓글 조회 (수정 필요) */
     @GET("/board/{boardId}/comment")
     suspend fun getComments(
         @Path("boardId") id: Int,
         @Query("page") page: Int
     )
 
-    /* 글 저장 -> 헤더에 토큰 TODO: 파라미터 수정 필요 */
+    /* 글 등록 -> 헤더에 토큰 TODO: 파라미터 수정 필요 */
     @POST("/board/save")
-    suspend fun savePost()
+    suspend fun writePost()
 
     @DELETE("/board/delete/{boardId}")
     suspend fun deletePost(
         @Path("boardId") id: Int
+    )
+
+    /* 글 저장 */
+    @POST("/board/like/{boardId}")
+    suspend fun savePost(
+        @Path("boardId") id: Int
+    )
+
+    /* 장소 저장 */
+    @POST("/board/saveBoardPlace/{boardPlaceId}")
+    suspend fun savePlace(
+        @Path("boardPlaceId") id: Int
     )
 
 
