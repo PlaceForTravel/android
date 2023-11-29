bc2a0aa9003c10ca5c27c1fe9d76f1df4c3830da
easyhz <jiton3@naver.com>
2023-11-30 00:13:58 +0900

    update: FeedRepositoryTest 추가

---
+++ app/src/test/java/com/easyhz/placeapp/domain/repository/FeedRepositoryTest.kt
@@ -0,0 +1,34 @@
+package com.easyhz.placeapp.domain.repository
+
+import com.easyhz.placeapp.api.FeedService
+import com.easyhz.placeapp.domain.model.feed.Content
+import com.easyhz.placeapp.domain.model.feed.Feed
+import com.easyhz.placeapp.domain.model.feed.Pageable
+import com.easyhz.placeapp.domain.model.feed.SortX
+import com.easyhz.placeapp.domain.repository.feed.FeedRepositoryImpl
+import kotlinx.coroutines.runBlocking
+import retrofit2.Response
+import org.junit.Test
+import org.mockito.Mockito.mock
+import org.mockito.Mockito.`when`
+
+class FeedRepositoryTest {
+
+    private var feedService: FeedService = mock(FeedService::class.java)
+    private var repository = FeedRepositoryImpl(feedService)
+
+    @Test
+    fun `fetch Feed Contents`() = runBlocking {
+        `when`(feedService.getFeed(1)).thenReturn(
+            Response.success(
+                Feed(
+                    listOf(Content(1, "서울", "", listOf(), 0,"", "", "", "", "", "")),
+                false, false, false, 0, 0, Pageable(0, 0, 0, true, SortX(true, true, true), false), 5, SortX(true, true, true), 0, 0
+                )
+            )
+        )
+
+        val result = repository.fetchFeedContents(1)
+        assert(result.isSuccessful)
+    }
+}
